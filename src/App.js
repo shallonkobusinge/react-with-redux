2e3146651983f6334dd43b822212bd2165f88a4b
Shallon Kobusinge <kobshallon041@gmail.com>
2021-08-04 14:30:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Post from './components/Post'
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <Post />
+    </div>
+  );
+};
+
+
+export default App;
---
+++ src/components/Post.js
@@ -0,0 +1,24 @@
+import React,{useEffect, useState} from 'react'
+
+function Post() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(res => res.json())
+            .then(data => setPosts(data))
+    }, [])
+    return (
+        <div>
+            <h2>Posts</h2>
+            {posts.map((post) => {
+                return(
+                <div key={post.id}>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                </div>
+                )
+            })}
+        </div>
+    )
+}
+export default Post
