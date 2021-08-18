1c3b18aebaf1d1317f4e5f154d33ab8571484cb1
Shallon Kobusinge <kobshallon041@gmail.com>
2021-08-18 22:29:26 +0200

    transform result

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import {compose, pipe} from 'lodash';
 function App() {
   let input = "Javascript"
   let output = "<div>"+input.trim()+"</div>"
   const trim = str => str.trim()
   const wrapInDiv = str => `<div>${str}</div>`
-  
+  const toLowerCase = str => str.toLowerCase()
+
+  const transform = pipe(trim,toLowerCase,wrapInDiv)
+  transform(input)
   return (
       <div>
         
