54e30bdeb7926382a3c7d568fa06fe27fede90e4
Shallon Kobusinge <kobshallon041@gmail.com>
2021-08-18 22:43:23 +0200

    currying

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ function App() {
   let input = "Javascript"
   let output = "<div>"+input.trim()+"</div>"
   const trim = str => str.trim()
-  const wrapInDiv = str => `<div>${str}</div>`
+  const wrap = type=>str => `<${type}>${str}</${type}>`
   const toLowerCase = str => str.toLowerCase()
 
-  const transform = pipe(trim,toLowerCase,wrapInDiv)
+  const transform = pipe(trim,toLowerCase,wrap("div"))
   transform(input)
   return (
       <div>
