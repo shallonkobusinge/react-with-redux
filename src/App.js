4966fc9e63c4cc88222d2a8ce2b56fba39999acb
Shallon Kobusinge <kobshallon041@gmail.com>
2021-08-18 23:22:21 +0200

    updating an object

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import {compose, pipe} from 'lodash/fp';
+import { compose, pipe } from 'lodash/fp';
 function App() {
-  let input = "Javascript"
-  let output = "<div>"+input.trim()+"</div>"
-  const trim = str => str.trim()
-  const wrap = type=>str => `<${type}>${str}</${type}>`
-  const toLowerCase = str => str.toLowerCase()
+  // let input = "Javascript"
+  // let output = "<div>"+input.trim()+"</div>"
+  // const trim = str => str.trim()
+  // const wrap = type=>str => `<${type}>${str}</${type}>`
+  // const toLowerCase = str => str.toLowerCase()
 
-  const transform = pipe(trim,toLowerCase,wrap("div"))
-  transform(input)
+  // const transform = pipe(trim,toLowerCase,wrap("div"))
+  // transform(input)
+  const person = {
+    name: "Shallon",
+    address: {
+      country: "United States",
+      city: "New York"
+    }
+  }
+  const updated = [{
+    name: "Shallon",
+    address: {
+      country: "United States",
+      city: "New York"
+    }  
+  
+  }]  // console.log(updated)
   return (
-      <div>
-        <h2>{transform(input)}</h2>
-      </div>
-    );
+    <div >
+      {updated.map((one,index) => {
+        return (
+          <div key={index}>
+          <h2>{one}</h2>
+          </div>
+        )
+      })}
+
+    </div>
+  );
 };
 
 
