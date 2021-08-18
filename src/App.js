7bf3c0174231e5a92e867d33c3d4aaaf371bd74d
Shallon Kobusinge <kobshallon041@gmail.com>
2021-08-18 22:28:16 +0200

    functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
 function App() {
-
+  let input = "Javascript"
+  let output = "<div>"+input.trim()+"</div>"
+  const trim = str => str.trim()
+  const wrapInDiv = str => `<div>${str}</div>`
+  
   return (
-      
+      <div>
+        
+      </div>
     );
 };
 
